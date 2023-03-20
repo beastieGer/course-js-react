b5b86cdcdcbc83f3ee800210fe09b7d55127c776
oleg <example@gmail.com>
2023-03-20 14:41:11 +0200

    task 012

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+const film1 = prompt("Один из последних просмотренных фильмов?", ""),
+	ball1 = prompt("На сколько оцените его", ""),
+	film2 = prompt("Один из последних просмотренных фильмов?", ""),
+	ball2 = prompt("На сколько оцените его", "");
+
+personalMovieDB.movies[film1] = ball1;
+personalMovieDB.movies[film2] = ball2;
+
+console.log(personalMovieDB);
